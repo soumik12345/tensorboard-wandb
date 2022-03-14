0aa40c31544345aa3a17195275fb23164853e360
soumik12345 <19soumik.rakshit96@gmail.com>
2022-03-14 21:34:45 +0530

    updated train script

--- train.py
+++ train.py
@@ -13,8 +13,16 @@ wandb.init(project="tensorboard-demo", sync_tensorboard=True)
 
 config = wandb.config
 config.labels = [
-    "T-shirt/top", "Trouser", "Pullover", "Dress",
-    "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
 ]
 config.input_size = 28
 config.dense_layer_units = 32
@@ -27,30 +35,27 @@ config.tensorboard_log_dir = os.path.join(
     "logs", datetime.now().strftime("%Y%m%d-%H%M%S")
 )
 
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.fashion_mnist.load_data()
+(train_images, train_labels), (
+    test_images,
+    test_labels,
+) = tf.keras.datasets.fashion_mnist.load_data()
 
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(
-        input_shape=(config.input_size, config.input_size)
-    ),
-    tf.keras.layers.Dense(
-        config.dense_layer_units, activation='relu'
-    ),
-    tf.keras.layers.Dropout(config.dropout_rate),
-    tf.keras.layers.Dense(
-        len(config.labels), activation='softmax'
-    )
-])
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Flatten(input_shape=(config.input_size, config.input_size)),
+        tf.keras.layers.Dense(config.dense_layer_units, activation="relu"),
+        tf.keras.layers.Dropout(config.dropout_rate),
+        tf.keras.layers.Dense(len(config.labels), activation="softmax"),
+    ]
+)
 
 model.compile(
-    optimizer=tf.keras.optimizers.Adam(
-        learning_rate=config.learning_rate
-    ),
+    optimizer=tf.keras.optimizers.Adam(learning_rate=config.learning_rate),
     loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-    metrics=['accuracy']
+    metrics=["accuracy"],
 )
 
 tensorboard_callback = tf.keras.callbacks.TensorBoard(
@@ -63,12 +68,12 @@ wandb_callback = WandbCallback(
 
 model.fit(
     train_images,
-    train_labels, 
+    train_labels,
     batch_size=config.batch_size,
     validation_data=(test_images, test_labels),
     validation_batch_size=config.validation_batch_size,
-    epochs=config.epochs, 
-    callbacks=[tensorboard_callback, wandb_callback]
+    epochs=config.epochs,
+    callbacks=[tensorboard_callback, wandb_callback],
 )
 
 wandb.finish()
